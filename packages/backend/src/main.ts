c21b5d8eb9e7725871d1330b8843bf1f9d8475fe
Alberto Cubero Navas <albertocuberonavas@gmail.com>
2024-10-28 20:19:13 +0100

    Backend: api routing

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -7,6 +7,11 @@ const app = new Hono();
 
 app.route('/v1', v1Router);
 
+app.onError((err, c) => {
+  console.error(err);
+  return c.text('Internal Server Error', 500);
+});
+
 serve(app, (info) => {
   console.log(`Listening on http://localhost:${info.port}`);
 });
