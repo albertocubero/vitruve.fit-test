8e02dec5d4ea2688c8782274ce14b492c454b083
Alberto Cubero Navas <albertocuberonavas@gmail.com>
2024-11-01 18:25:48 +0100

    Backend: add CORS

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,12 @@
 import { Hono, Context } from 'hono';
 import { serve } from '@hono/node-server';
-import logger from './utils/logger';
+import { cors } from 'hono/cors'
 import apiRouterV1 from './api/v1/infrastructure/routes/routes';
+import logger from './utils/logger';
 
 const app = new Hono();
 
+app.use('/api/v1/*', cors())
 app.route('/api/v1', apiRouterV1);
 
 app.onError((err: Error, c: Context)  => {
